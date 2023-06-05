9474e333caf92ffd2cb4e9e97d0a4fad498d917e
alexxbout <boutinaud.pro@gmail.com>
2023-06-05 16:04:18 +0200

    Added user's interests on profile

---
+++ src/components/InterestCardProfil.vue
@@ -0,0 +1,32 @@
+<template>
+    <!-- <div class="flex items-center justify-center w-full h-max gap-x-5"> -->
+        <div :style="{'border-color': color, 'color': color}" class="flex flex-col items-center justify-center w-full border-[3px] rounded-2xl h-max gap-y-2 p-2">
+            <Emoji :data="{ name: props.data.emoji, size: 'PROFILE' }" />
+            <span class="font-medium">{{ props.data.name }}</span>
+        </div>
+    <!-- </div> -->
+</template>
+
+<script setup lang="ts">
+import { PropType, ref} from "vue";
+
+import type { IInterest } from "../types/Interest";
+
+import Emoji from "./Emoji.vue";
+
+// ########################################### VARIABLES ###########################################
+
+const props = defineProps({
+    data: {
+        type: Object as PropType<IInterest>,
+        required: true
+    }
+});
+
+const color = ref<string>(props.data.color ? props.data.color : "#000000");
+
+// ########################################### EVENTS ###########################################
+
+// ########################################### HANDLERS ###########################################
+
+</script>
--- src/router/App/Profile.vue
+++ src/router/App/Profile.vue
@@ -1,18 +1,19 @@
 <template>
     <div class="relative flex flex-col mb-20 gap-y-16 margins">
 
-        <div class="flex items-center w-full gap-y-2">
-            <div class="flex gap-x-[15px] items-center justify-center w-max">
+        <!-- Header -->
+        <div class="flex items-center w-full">
+            <div class="flex gap-x-[15px] items-center justify-between w-max">
                 <i class="text-3xl bi bi-person-circle"></i>
                 <span class="header">Profil</span>
             </div>
         </div>
 
-        <!-- Header : Picture + fullname + settings -->
-        <div class="flex gap-x-5 items-center justify-center w-max">
+        <!-- Picture + fullname + settings -->
+        <div class="flex items-center justify-center gap-x-5 w-max">
             <UserProfilPicture :data="{ pic: user?.pic, style: 'LARGE' }" />
 
-            <div class="flex flex-col gap-y-5 font-medium text-3xl">
+            <div class="flex flex-col text-3xl font-medium gap-y-5">
                 <span>{{ user?.firstname + ' ' + user?.lastname }}</span>
 
                 <Button :data="{ apparence: { color: 'BLUE', size: 'XS', type: 'SECONDARY', rounded: 'FULL' }, text: 'Paramètres du compte' }" />
@@ -20,20 +21,28 @@
         </div>
 
         <!-- Bio -->
-        <div class="flex flex-col gap-y-5">
-            <span class="font-medium text-xl">Bio</span>
+        <div class="flex flex-col gap-y-3">
+            <span class="text-xl font-medium">Bio</span>
             <span class="text-base text-gray-400">{{ user?.bio }}</span>
         </div>
 
         <!-- Friends -->
-        <div class="flex flex-col gap-y-5">
-            <span class="font-medium text-xl">Amis</span>
+        <div class="flex flex-col gap-y-3">
+            <span class="text-xl font-medium">Amis</span>
             <div class="flex items-center justify-between w-full">
                 <div class="flex items-center justify-center w-max">
                     <UserProfilPicture class="-mx-2" v-for="friend in friends" :data="{ pic: friend?.pic, style: 'SMALL' }" />
                 </div>
 
                 <Button :data="{apparence: { color: 'BLUE', size: 'XS', type: 'SECONDARY', rounded: 'FULL'}, text: 'Voir plus'}" />
+            </div>
+        </div>
+
+        <!-- Interests -->
+        <div class="flex flex-col gap-y-3">
+            <span class="text-xl font-medium">Centres d’intérêts</span>
+            <div style="grid-auto-columns: 1fr;" class="grid w-full grid-flow-col gap-x-3">
+                <InterestCardProfil v-for="interest in interests" :data="interest"  />
             </div>
         </div>
     </div>
@@ -46,19 +55,28 @@ import UserProfilPicture from "../../components/UserProfilPicture.vue";
 import UtilsAuth from "../../utils/UtilsAuth";
 import type { IUser } from "../../types/User";
 import UtilsApi from "../../utils/UtilsApi";
+import InterestCardProfil from "../../components/InterestCardProfil.vue";
+import { IInterest } from "../../types/Interest";
 
 const MAX_FRIENDS = 5;
 const user = UtilsAuth.getCurrentUser();
 
 const friends = ref<IUser[]>([]);
+const interests = ref<IInterest[]>([]);
 
 onMounted(async () => {
-    let data = await UtilsApi.getAllFriends(user!.id);
+    // Get all friends
+    const dataFriends = await UtilsApi.getAllFriends(user!.id);
 
-    console.log(data);
+    if (dataFriends) {
+        friends.value = dataFriends.slice(0, MAX_FRIENDS);
+    }
 
-    if (data) {
-        friends.value = data.slice(0, MAX_FRIENDS);
+    // Get all interests
+    const dataInterests = await UtilsApi.getUserInterests(user!.id);
+
+    if (dataInterests) {
+        interests.value = dataInterests;
     }
 });
 </script>
